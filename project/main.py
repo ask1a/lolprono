ddb4069c5cc5db7ef9644ed3624124b5aa0c4efd
Ask1a <skiaa@hotmail.com>
2024-04-11 19:36:44 +0200

    manage updating game

--- project/main.py
+++ project/main.py
@@ -4,7 +4,7 @@ from . import db
 from .models import UserLeague, Game
 import pandas as pd
 import io
-from datetime import datetime
+from datetime import datetime, timedelta
 
 main = Blueprint('main', __name__)
 
@@ -20,6 +20,7 @@ def profile():
     current_user_league_list = [e.leaguename for e in
                                 UserLeague.query.filter_by(userid=current_user.id).order_by(UserLeague.leagueid).all()]
     return render_template('profile.html', name=current_user.name, league_list=current_user_league_list)
+
 
 @main.route('/profile_unsubscribe_league/<leaguename>', methods=['POST'])
 @login_required
@@ -37,14 +38,15 @@ def profile_unsubscribe_league_post(leaguename):
 def admin():
     return render_template('admin.html')
 
+
 @main.route('/admin_add_games', methods=['POST'])
 def admin_add_games():
     if 'gamesdata' not in request.files:
         return redirect(url_for('main.admin'))
     file = request.files['gamesdata']
-    if file.filename=='':
+    if file.filename == '':
         return redirect(url_for('main.admin'))
-    if file.filename[-3:]!='csv':
+    if file.filename[-3:] != 'csv':
         return redirect(url_for('main.admin'))
 
     file_stream = io.BytesIO(file.read())
@@ -52,12 +54,15 @@ def admin_add_games():
 
     for game in games_to_load_in_database:
         # check if game already exist then delete to overwrite:
-        row = Game.query.filter_by(
-            gamedatetime=datetime.fromisoformat(game['gamedatetime']),
-            team1=game['team1'],
-            team2=game['team2']
+        check_exist = {}
+        row = Game.query.filter(
+            Game.gamedatetime <= datetime.fromisoformat(game['gamedatetime']) + timedelta(minutes=1),
+            Game.gamedatetime >= datetime.fromisoformat(game['gamedatetime']) - timedelta(minutes=1),
+            Game.team1 == game['team1'],
+            Game.team2 == game['team2']
         ).first()
         if row:
+            check_exist['gameid'] = row.id
             print("suppression enregistrement")
             # delete the game in database
             db.session.delete(row)
@@ -73,9 +78,10 @@ def admin_add_games():
             team1score=game['team1score'],
             team2score=game['team2score']
         )
+        if check_exist.get('gameid'):
+            new_game.id = check_exist.get('gameid')
         # add the new user to the database
         db.session.add(new_game)
         db.session.commit()
-
 
     return render_template('admin.html')
