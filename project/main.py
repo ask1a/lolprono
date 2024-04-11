53a1322f17aa97f7458e53b98d888ce6bfada928
Ask1a <skiaa@hotmail.com>
2024-04-11 19:03:32 +0200

    admin page

--- project/main.py
+++ project/main.py
@@ -1,7 +1,10 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 from . import db
-from .models import UserLeague
+from .models import UserLeague, Game
+import pandas as pd
+import io
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -28,3 +31,51 @@ def profile_unsubscribe_league_post(leaguename):
     db.session.commit()
 
     return redirect(url_for('main.profile'))
+
+
+@main.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@main.route('/admin_add_games', methods=['POST'])
+def admin_add_games():
+    if 'gamesdata' not in request.files:
+        return redirect(url_for('main.admin'))
+    file = request.files['gamesdata']
+    if file.filename=='':
+        return redirect(url_for('main.admin'))
+    if file.filename[-3:]!='csv':
+        return redirect(url_for('main.admin'))
+
+    file_stream = io.BytesIO(file.read())
+    games_to_load_in_database = pd.read_csv(file_stream).to_dict("records")
+
+    for game in games_to_load_in_database:
+        # check if game already exist then delete to overwrite:
+        row = Game.query.filter_by(
+            gamedatetime=datetime.fromisoformat(game['gamedatetime']),
+            team1=game['team1'],
+            team2=game['team2']
+        ).first()
+        if row:
+            print("suppression enregistrement")
+            # delete the game in database
+            db.session.delete(row)
+            db.session.commit()
+
+        # create a game with the csv data.
+        new_game = Game(
+            leagueid=game['leagueid'],
+            bo=game['bo'],
+            gamedatetime=datetime.fromisoformat(game['gamedatetime']),
+            team1=game['team1'],
+            team2=game['team2'],
+            team1score=game['team1score'],
+            team2score=game['team2score']
+        )
+        # add the new user to the database
+        db.session.add(new_game)
+        db.session.commit()
+
+
+    return render_template('admin.html')
