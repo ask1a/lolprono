f45a76b5a5261a9befac17a25874717867d6f720
GeoffreyRoigDeslandes <157515848+GeoffreyRoigDeslandes@users.noreply.github.com>
2024-04-28 15:38:48 +0200

    Final version candidate
    * transform utils to create a class
    * add first batch of unit tests.

--- jobs/insert_new_games.py
+++ jobs/insert_new_games.py
@@ -1,7 +1,7 @@
 import utils 
 
 if __name__ == '__main__':
-    df = utils.get_game_schedule_dataframe()
-    df_clean = utils.clean_schedule(df)
-    utils.insert_future_games(df_clean)
-    
+    scrap = utils.Scrap()
+    df_raw = scrap.get_game_schedule_dataframe()
+    df_clean =scrap.clean_schedule(df_raw)
+    scrap.insert_future_games(df_clean)
--- jobs/update_game_results.py
+++ jobs/update_game_results.py
@@ -1,6 +1,7 @@
 import utils
 
 if __name__ == '__main__':
-    df = utils.get_game_results_dataframe()
-    df_clean = utils.clean_results(df)
-    utils.update_game_results(df_clean)
+    scrap = utils.Scrap()
+    df_raw = scrap.get_game_results_dataframe()
+    df_clean =scrap.clean_results(df_raw)
+    scrap.update_game_results(df_clean)
