ac8a53ce08fef7046f826c74c81338e8cacafe92
Askia VANRYCKEGHEM <askia.vanryckeghem@external.engie.com>
2024-06-01 13:29:42 +0200

    discord webhooks in env

--- project/discord_webhooks.py
+++ project/discord_webhooks.py
@@ -1,6 +1,9 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-url_wh = "https://discord.com/api/webhooks/1231251899680952380/i2N0SvUkz4xMxUMD3f5bzmi0u8OfYwFv-FG6kgWwvJDKkMKYGM43TVdBLNKCY5PUR7E6"
+url_wh = os.getenv('DISCORD_WH')
+
 
 payload = {'content':'Des nouveaux matchs sont disponibles au pronostics sur LolProno'}
 response = requests.post(url_wh, json=payload)
