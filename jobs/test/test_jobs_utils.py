dbbb22a1d85e16099ef231f333123a29fd69d9c8
GeoffreyRoigDeslandes <157515848+GeoffreyRoigDeslandes@users.noreply.github.com>
2025-04-21 11:54:45 +0200

    Update test_jobs_utils.py

--- jobs/test/test_jobs_utils.py
+++ jobs/test/test_jobs_utils.py
@@ -30,14 +30,14 @@ def test_check_league():
 
 def test_assign_league_id():
     # example DataFrame for tests
-    data = {'id': [3, 1, 2, 4], 'leaguename': ['MSI 2024', 'LEC spring 2024', 'LEC summer 2024', 'LEC Spring 2025']}
+    data = {'id': [3, 4], 'leaguename': ['Mid-Season Invitational 2024', 'LEC Spring 2025']}
     # Declaring class
     test_panda = utils.PandaScoreRequest(test_job=True)
     test_panda.leagues = pd.DataFrame(data)
     # Test case 1 : corresponding
-    assert test_panda.assign_league_id('LEC spring 2024') == 1
+    assert test_panda.assign_league_id('LEC spring 2024') == -1
     # Test case 2 : partial
-    assert test_panda.assign_league_id('MSI 2024 playin') == 3
+    assert test_panda.assign_league_id('Mid-Season Invitational 2024') == 3
     # Test case 2bis : barely correspond
     assert test_panda.assign_league_id('MSI 202') == -1
     # Test case 3 : no match
@@ -52,10 +52,22 @@ def test_identifty_team_names(expected_df_for_clean_schedule):
     test_panda = utils.PandaScoreRequest(test_job=True)
     test_df = pd.DataFrame(
         {
-            'leagueid': [3, 3, 3, 3], 'bo': [3, 3, 3, 3],
-            'game_datetime': ['2024-05-01 08:00:00', '2024-05-01 11:00:00', '2024-05-02 08:00:00',
-            '2024-05-02 11:00:00'], 'team_1': ['FLY', 'T1', 'FNC', 'TES'],
-            'team_2': ['PSG', 'EST', 'GAM', 'LLL']
+            'leagueid': [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4],
+            'bo': [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+            'game_datetime': ['2025-04-20 15:00:00', '2025-04-20 17:00:00', '2025-04-21 15:00:00', '2025-04-21 17:00:00',
+                '2025-04-26 13:00:00', '2025-04-26 15:00:00', '2025-04-27 13:00:00', '2025-04-27 15:00:00',
+                '2025-04-28 15:00:00', '2025-04-28 17:00:00', '2025-05-03 15:00:00', '2025-05-03 17:00:00',
+                '2025-05-04 15:00:00', '2025-05-04 17:00:00', '2025-05-05 15:00:00', '2025-05-05 17:00:00',
+                '2025-05-10 15:00:00', '2025-05-10 17:00:00', '2025-05-11 15:00:00', '2025-05-11 17:00:00',
+                '2025-05-12 15:00:00', '2025-05-12 17:00:00'],
+            'team_1': [
+                    'TH', 'G2', 'SK', 'MKOI', 'G2', 'MKOI', 'G2', 'MKOI', 'TH', "VIT",
+                    'FNC', 'KC', 'GX', 'G2', 'TH', 'MKOI', 'G2', 'KC', 'VIT', 'KC', 'TH', 'MKOI'
+                ],
+            'team_2': [
+                    'VIT', 'BDS', 'RGE', 'VIT', 'GX', 'FNC', 'FNC', 'GX', 'SK', 'BDS', 'BDS', 'RGE',
+                    'BDS', 'VIT', 'FNC', 'SK', 'SK', 'VIT', 'SK', 'G2', 'GX', 'RGE'
+                ]
         }
     )
     test_df = test_panda.identifty_team_names(test_df)
@@ -126,7 +138,7 @@ def expected_df_for_clean_schedule():
             'team_1':[
                     'Team Heretics', 'G2 Esport', 'SK Gaming', 'Movistar KOI', 'G2 Esport', 'Movistar KOI', 'G2 Esport', 'Movistar KOI',
                     'Team Heretics', "Vitality", 'Fnatic', 'Karmine Corp', 'Giant X', 'G2 Esport', 'Team Heretics', 'Movistar KOI', 'G2 Esport',
-                    'Karmine Corp', 'Vitality', 'Karmine Corp', 'Team Heretics', 'Movistar Koi'
+                    'Karmine Corp', 'Vitality', 'Karmine Corp', 'Team Heretics', 'Movistar KOI'
                 ],
             'team_2':  [
                     'Vitality', 'Team BDS', 'Rogue', 'Vitality', 'Giant X', 'Fnatic', 'Fnatic', 'Giant X', 'SK Gaming', 'Team BDS', 'Team BDS', 'Rogue',
@@ -151,7 +163,7 @@ def expected_df_for_clean_results():
             'team_1':[
                     'Team Heretics', 'G2 Esport', 'SK Gaming', 'Movistar KOI', 'G2 Esport', 'Movistar KOI', 'G2 Esport', 'Movistar KOI',
                     'Team Heretics', "Vitality", 'Fnatic', 'Karmine Corp', 'Giant X', 'G2 Esport', 'Team Heretics', 'Movistar KOI', 'G2 Esport',
-                    'Karmine Corp', 'Vitality', 'Karmine Corp', 'Team Heretics', 'Movistar Koi'
+                    'Karmine Corp', 'Vitality', 'Karmine Corp', 'Team Heretics', 'Movistar KOI'
                 ],
             'team_2':  [
                     'Vitality', 'Team BDS', 'Rogue', 'Vitality', 'Giant X', 'Fnatic', 'Fnatic', 'Giant X', 'SK Gaming', 'Team BDS', 'Team BDS', 'Rogue',
@@ -197,7 +209,7 @@ def expected_df_for_get_game_schedule_dataframe():
                 ],
             'team_1': [
                     'TH', 'G2', 'SK', 'MKOI', 'G2', 'MKOI', 'G2', 'MKOI', 'TH', "VIT",
-                    'FNC', 'KC', 'GX', 'G2', 'TH', 'MKOI', 'G2', 'KC', 'VIT', 'KC', 'TH', 'MKOIs'
+                    'FNC', 'KC', 'GX', 'G2', 'TH', 'MKOI', 'G2', 'KC', 'VIT', 'KC', 'TH', 'MKOI'
                 ],
             'team_2': [
                     'VIT', 'BDS', 'RGE', 'VIT', 'GX', 'FNC', 'FNC', 'GX', 'SK', 'BDS', 'BDS', 'RGE',
