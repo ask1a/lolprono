09b14c0ab2d0bcbd40b6174d55365781a4df57e4
GeoffreyRoigDeslandes <157515848+GeoffreyRoigDeslandes@users.noreply.github.com>
2025-07-03 19:32:14 +0200

    Fix al issue  (#52)
    * Update utils.py
    
    Fix f string by changing single quotes by double quotes
    
    * Update utils.py

--- jobs/utils.py
+++ jobs/utils.py
@@ -555,8 +555,8 @@ class PandaScoreRequest:
                 leagueid = {league_id}
                 AND bo = {bo}
                 AND date(game_datetime) = date('{game_date}')
-                AND team_1 = '{team_1}'
-                AND team_2 = '{team_2}';
+                AND team_1 = "{team_1}"
+                AND team_2 = "{team_2}";
             """
             self.cursor.execute(query)
             self.conn.commit()
