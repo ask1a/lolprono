aabd631764a8be362e171fb04a37948ca1cbe8bc
GeoffreyRoigDeslandes <157515848+GeoffreyRoigDeslandes@users.noreply.github.com>
2025-07-31 20:13:57 +0200

    LEC Summer 2025 (#54)
    * LEC Summer 2025
    
    * Update auth.py
    
    * to_jpg

--- project/auth.py
+++ project/auth.py
@@ -145,7 +145,7 @@ def lost_password_post():
 def ligues():
     return render_template('ligues.html',
                            league1=is_registered_in_league(1),
-                           league2=is_registered_in_league(2),
+                           league2=is_registered_in_league(6),
                            league3=is_registered_in_league(3),
                            league4=is_registered_in_league(4),
                            league5=is_registered_in_league(5))
@@ -188,8 +188,8 @@ def ligue_spring_post():
 @login_required
 def ligue_summer_post():
     userid = current_user.id
-    leagueid = 2
-    leaguename = "LEC summer 2024"
+    leagueid = 6
+    leaguename = "LEC Summer 2025"
 
     return add_userleague_row(leagueid, leaguename, userid)
 
